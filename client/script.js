81641d359f92fa07fc603f22908d81ee6559df13
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2021-05-29 13:49:55 +0530

    Created Frontend
    Frontend created. CodeMirror used for text editor

---
+++ client/script.js
@@ -0,0 +1,29 @@
+async function compileIt() {
+  document.getElementById("opscreen").style.visibility = "visible";
+  var response = await fetch("https://codeorbored.herokuapp.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: JSON.stringify({
+      code: document.querySelector(".CodeMirror").CodeMirror.getValue(),
+      language: document.querySelector("#language").value,
+      standardIn: document
+        .querySelector("#stdin")
+        .value.split(/[\s,]+/)
+        .join("\n")
+    })
+  })
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      document.querySelector("#output").innerHTML = data.output;
+    })
+    .catch(error => alert(error.message));
+}
+
+document.querySelector("#compile").addEventListener("click", e => {
+  compileIt();
+});
