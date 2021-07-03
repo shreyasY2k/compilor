a5f4a4c39f81b9c38183a25d50bd5f6ee9235766
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2021-07-03 23:58:57 +0530

    Add files via upload

--- client/script.js
+++ client/script.js
@@ -3,7 +3,7 @@ async function compileIt() {
   var response = await fetch("http://127.0.0.1:3000", {
     method: "POST",
     headers: {
-      "Content-Type": "text/plain"
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
       code: document.querySelector(".CodeMirror").CodeMirror.getValue(),
